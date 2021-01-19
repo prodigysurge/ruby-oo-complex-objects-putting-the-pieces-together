1362dcb8344f01a627eeab40a287f4ebf8a1cfbd
prodigysurge <boatinglife6@yahoo.com>
2021-01-19 18:37:08 -0500

    commit tree 8621739e235c45456b40f099ec18aa1dbc659366

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,78 @@
-# Make your shoe class here!
+require 'pry'
+
+class Shoe
+
+    def initialize(brand)
+         @brand = brand
+    end
+
+    def brand
+        @brand
+    end
+
+    def color=(color)
+        @color = color
+    end
+
+    def color
+        @color
+    end
+
+    def size=(size)
+        @size = size
+    end
+
+    def size
+        @size
+    end
+
+    def material=(material)
+        @material = material
+    end
+
+    def material
+        @material
+    end
+
+    def condition=(condition)
+        @condition = condition
+    end
+
+    def condition
+        @condition
+    end
+
+    def cobble=(cobble)
+        @cobble 
+    end
+
+    def cobble
+        self.condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+
+    
+
+    # def new_condition=(new_condition)
+    #     @new_condition = new_condition
+    # end
+
+    # def new_condition
+        
+
+    # def cobble
+    #     shoe.condition = "new"
+    # end
+
+
+
+
+end
+
+
+
+
+
+
+
+
